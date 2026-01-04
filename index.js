40a7ec586bcd70d0d4a96206803ef69a0d91481a
Rabii <rabii.myself@gmail.com>
2026-01-04 21:16:21 +0100

    v1.3 fixed some bugs & added functionality to the booking button with a modal overlay containing contact information

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('navlinks');
 
-menuToggle.addEventListener("click", () => 
-    {
-        navMenu.classList.toggle('active');
+const bookBtn = document.querySelectorAll('.bookBtn');
+const overlay = document.getElementById('overlay');
+const closeBtn = document.getElementById('closeModal');
+const modal = document.getElementById('bookingModal');
+
+bookBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        overlay.classList.add("active");
+        modal.classList.add("active");
+        document.body.classList.add("modal-open");
     });
+});
+
+closeBtn.onclick = () => {
+    overlay.classList.remove("active");
+    modal.classList.remove("active");
+    document.body.classList.remove("modal-open");
+};
+
+menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle('active');
+});
